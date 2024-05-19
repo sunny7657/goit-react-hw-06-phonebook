0d1e11d43ad47352b9f52ec59cc79e64bf4aa0c2
sunny7657 <ksvetlana466@gmail.com>
2024-05-19 16:41:41 +0200

    upd

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 // import { Recipe } from './Recipe/Recipe';
-// import recipes from '../recepies.json';
+// import recipes from '../recipes.json';
 
 // export const App = () => {
 //   return (
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,10 +1,10 @@
 import { ContactList } from 'components/ContactList/ContactList';
-import { useState } from 'react';
+// import { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const Contacts = () => {
   const contacts = useSelector(store => store.contacts);
-  const [filter, setFilter] = useState('');
+  // const [filter, setFilter] = useState('');
   return <ContactList items={contacts} />;
 };
 
