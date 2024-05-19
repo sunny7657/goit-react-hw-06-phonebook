b14ba943137d55146c8bd7029c8f80355d28c055
sunny7657 <ksvetlana466@gmail.com>
2024-05-19 16:38:23 +0200

    upd

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
-import { Recipe } from './Recipe/Recipe';
-import recipes from '../recepies.json';
+// import { Recipe } from './Recipe/Recipe';
+// import recipes from '../recepies.json';
 
-export const App = () => {
-  return (
-    <>
-      {recipes.map((recipe, idx) => (
-        <Recipe key={idx} recipe={recipe} />
-      ))}
-    </>
-  );
-};
-
-App.
+// export const App = () => {
+//   return (
+//     <>
+//       {recipes.map((recipe, idx) => (
+//         <Recipe key={idx} recipe={recipe} />
+//       ))}
+//     </>
+//   );
+// };
