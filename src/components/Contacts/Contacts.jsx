39f07ac60b4e5bd6785fa6ed76f39ca2d9d09f01
sunny7657 <ksvetlana466@gmail.com>
2024-05-19 23:04:29 +0200

    upd

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 const Contacts = () => {
   const contacts = useSelector(store => store.contacts);
   const dispatch = useDispatch();
+  console.log(dispatch);
   // const [filter, setFilter] = useState('');
   return <ContactList items={contacts} />;
 };
